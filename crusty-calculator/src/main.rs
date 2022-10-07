90fbac6a6c8c357d0bde8bd9ec7dec417610bca2
root <root@DESKTOP-MABOOE.redmond.corp.microsoft.com>
2022-10-07 15:48:08 -0700

    Update with separate library and binary

---
+++ crusty-calculator/src/main.rs
@@ -0,0 +1,9 @@
+use crusty_math::{add, generate_random_integer};
+
+fn main() {
+    println!("Hello, world!");
+
+    println!("1 + 1 = {}", add(1, 1,));
+
+    println!("A random integer is {}", generate_random_integer());
+}
---
+++ crusty-math/src/lib.rs
@@ -0,0 +1,22 @@
+use rand::{self, random};
+
+pub fn generate_random_integer() -> i32 {
+    random()
+}
+
+// Basic auto-generated examples below
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
